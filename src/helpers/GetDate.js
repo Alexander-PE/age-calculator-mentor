751d477bedd8cb432df5d55ccc6a38e71dcbe947
Alexander Perez <alexander.pereze02@gmail.com>
2023-05-25 15:00:28 -0400

    some changes e.e

--- src/helpers/GetDate.js
+++ src/helpers/GetDate.js
@@ -1,15 +1,15 @@
 export function GetDate(fecha) {
-  let fechaActual = new Date();
-  let fechaPasada = new Date(fecha);
+  let today = new Date();
+  let pastDate = new Date(fecha);
 
-  let year = fechaActual.getFullYear() - fechaPasada.getFullYear();
-  let month = fechaActual.getMonth() - fechaPasada.getMonth();
-  let days = fechaActual.getDate() - fechaPasada.getDate();
+  let year = today.getFullYear() - pastDate.getFullYear();
+  let month = today.getMonth() - pastDate.getMonth();
+  let days = today.getDate() - pastDate.getDate();
 
   if (days < 0) {
     month--;
-    let ultimosDiasMesPasado = new Date(fechaPasada.getFullYear(), fechaPasada.getMonth() + 1, 0).getDate();
-    days += ultimosDiasMesPasado;
+    let lastDaysMonthPasts = new Date(pastDate.getFullYear(), pastDate.getMonth() + 1, 0).getDate();
+    days += lastDaysMonthPasts;
   }
   if (month < 0) {
     year--;
